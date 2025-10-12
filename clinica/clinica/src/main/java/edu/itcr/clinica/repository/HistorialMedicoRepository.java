8338bfe3bf926420f54bb4de9db4bc54ccf7a733
CheuzEx <114528843+CheuzEx@users.noreply.github.com>
2025-10-12 12:13:40 -0600

    Update HistorialMedicoRepository.java

--- clinica/clinica/src/main/java/edu/itcr/clinica/repository/HistorialMedicoRepository.java
+++ clinica/clinica/src/main/java/edu/itcr/clinica/repository/HistorialMedicoRepository.java
@@ -10,12 +10,12 @@ import java.util.Optional;
 @Repository
 public interface HistorialMedicoRepository extends JpaRepository<HistorialMedico, Long> {
 
-    // ¿ya existe historial para esta cita?
+    
     boolean existsByCita_IdCita(Long idCita);
 
-    // traer historial de una cita específica
+    
     Optional<HistorialMedico> findByCita_IdCita(Long idCita);
 
-    // listar historiales de un paciente, más recientes primero
+    
     List<HistorialMedico> findByPaciente_IdPacienteOrderByFechaConsultaDesc(Long idPaciente);
 }
